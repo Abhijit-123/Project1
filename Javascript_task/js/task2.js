fda928b9f22ac95e64dd2c99fb8398ef4590cced
Abhijit-123 <abhijitmondal02000@gmail.com>
2021-06-08 23:50:24 +0530

    updated using by backtick

--- Javascript_task/js/task2.js
+++ Javascript_task/js/task2.js
@@ -1,15 +1,15 @@
 /* Question -2
 Declare all the variables in above form without initialising and print their value.
 */
-let Name = window.prompt("enter your name: ");
-let Email = window.prompt("enter your mail adress: ");
-let Age = window.prompt("enter your age: ");
-let Adress = window.prompt("enter your permanent adress: ");
-let PhnNo = window.prompt("enter your phone no.:");
-let Gender = window.prompt("enter your gender: ");
-document.write("your name is: ",Name,"<br>");
-document.write("your email is: ",Email,"<br>");
-document.write("your age is: ",Age,"<br>");
-document.write("your adress is: ",Adress,"<br>");
-document.write("your phn_no is: ",PhnNo,"<br>");
-document.write("your gender is: ",Gender,"<br>");
+let Name = window.prompt(`enter your name: `);
+let Email = window.prompt(`enter your mail adress: `);
+let Age = window.prompt(`enter your age: `);
+let Adress = window.prompt(`enter your permanent adress: `);
+let PhnNo = window.prompt(`enter your phone no.:`);
+let Gender = window.prompt(`enter your gender: `);
+document.write(`your name is: ${Name}<br>`);
+document.write(`your email is: ${Email}<br>`);
+document.write(`your age is: ${Age}<br>`);
+document.write(`your adress is: ${Adress}<br>`);
+document.write(`your phn_no is: ${PhnNo}<br>`);
+document.write(`your gender is: ${Gender}<br>`);
