ec7fdc376c1a8dcc53a90d3a69f5316c0f2096dc
Abhijit-123 <abhijitmondal02000@gmail.com>
2021-06-13 18:53:52 +0530

    use switch case

--- Javascript_task3/js/task3.js
+++ Javascript_task3/js/task3.js
@@ -2,21 +2,46 @@
 Use switch case to return a grade for the score, either "A", "B", "C", "D", or "F". (Take convension for numbers
 and grades by yourself)Anything below 50 is F.
 */
-let marksP = window.prompt(`enter marks percentage`);
-let marksParcent = parseInt(marksP);
+let marksParcent = window.prompt(`enter marks percentage`);
+marksParcent = Number(marksParcent);
+let grade;
+let result;
 
     if(marksParcent>80){
-        document.write(`Your obtain grade is A`);
+        grade='A';
     }
     else if(marksParcent>70 && marksParcent<=80){
-        document.write(`Your obtain grade is B`);
+        grade='B';
     }
     else if(marksParcent>60 && marksParcent<=70){
-        document.write(`Your obtain grade is C`);
+        grade='C';
     }
     else if(marksParcent>50 && marksParcent<=60){
-        document.write(`Your obtain grade is D`);
+        grade='D'
     }
     else{
-        document.write(`Your obtain grade is F`); 
+
+        grade='F'
     }
+    //Use Switch Case
+    switch(grade){  
+        case 'A':  
+        result="A Grade";  
+        break;  
+        case 'B':  
+        result="B Grade";  
+        break;  
+        case 'C':  
+        result="C Grade";  
+        break; 
+        case 'D':  
+        result="D Grade";  
+        break; 
+        case 'F':  
+        result="F Grade";  
+        break;  
+        default:  
+        result="No Grade";  
+        } 
+        
+    document.write(result);
