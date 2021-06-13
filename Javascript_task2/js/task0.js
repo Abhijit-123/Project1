be8df08bfa258e9a9355cf13ea52407b09745ac2
Abhijit-123 <abhijitmondal02000@gmail.com>
2021-06-13 19:23:50 +0530

    updated

---
+++ Javascript_task2/js/task0.js
@@ -0,0 +1,18 @@
+/* Question - 0
+In the variables declare above assign the variables as appropriate default value instead of null.
+*/
+let name;
+let email;
+let age;
+let adress;
+let phnNo;
+let gender;
+let isMarried;
+
+document.write(`${name} <br>`);
+document.write(`${email} <br>`);
+document.write(`${age} <br>`);
+document.write(`${adress} <br>`);
+document.write(`${phnNo} <br>`);
+document.write(`${gender} <br>`);
+document.write(`${isMarried} <br>`);
